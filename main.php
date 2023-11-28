599fe0b5c69a4198a6d489cf238de89235b84d51
jhlackey <jlackey@uvm.edu>
2023-11-28 18:52:09 -0500

    No changes

---
+++ main.php
@@ -0,0 +1,8 @@
+//<?php
+//    include 'top.php';
+//?>
+
+<main>
+    <h2>Bruins 2011 Stanley Cup GOATs</h2>
+    <p>The Boston Bruins won the 2011 Stanley Cup, beating the Vancouver Canucks 4-3 games. In our opinion, the 2011 Bruins <strong>G</strong>reatest <strong>O</strong>f <strong>A</strong>ll <strong>T</strong>ime</b> players are Patrice Bergeron, Zdeno Chara, and  Tim Thomas (UVM alum).</p>
+</main>

3bdae3c77e0e88b097163c7c271edcf1fe885929
jhlackey <jlackey@uvm.edu>
2023-11-29 10:27:39 -0500

    Writing bio

--- chara.php
+++ chara.php
@@ -1,13 +1,21 @@
 <h1>Zdeno Chara</h1>
 
 <section id="image">
-
+<figure>
+    <img src="images/zdenoChara2.jpg" alt="2011 Stanley Cup Winner Zdeno Chara">
+    <figcaption><cite>Source: Getty Image</cite></figcaption>
+</figure>
 </section>
 
 <section id="bio">
+<h2>Biography</h2>
+
+    <p>Zdeno Chara, was born in 1977 in Slovakia. He played in the NHL for 24 seasons, from 1998-2022, playing for the New York Islanders, Ottawa Senators, Boston Bruins, and the Washington Capitals. His career began with the New York Islanders, but he spent a majority of his years with the Bruins, from 2006-2020. At the end of his career, he played for the New York Islanders, but in 2022 he signed a one-day contract with the Bruins, to official retire as a member of the team. During his time in the NHL, Chara set multiple notable records, such as tallest player in history at 6 foot 9 inches, most games played by a defenceman at .<br></p>
+</section>
+<section id="stats">
 
 </section>
 
-<section id="stats">
+<section id="video">
 
 </section>

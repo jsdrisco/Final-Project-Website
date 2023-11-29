53a79febc8a75f390226cc2226d8b3a95a66a33b
jhlackey <jlackey@uvm.edu>
2023-11-28 19:08:07 -0500

    Created nav and chara

---
+++ chara.php
@@ -0,0 +1,13 @@
+<h1>Zdeno Chara</h1>
+
+<section id="image">
+
+</section>
+
+<section id="bio">
+
+</section>
+
+<section id="stats">
+
+</section>
---
+++ nav.php
@@ -0,0 +1,5 @@
+<a href="bergeron.php">Bergeron</a>
+<a href="chara.php">Chara</a>
+<a href="thomas.php">Thomas</a>
+<a href="main.php">Home</a>
+<a href="form.php">Form</a>

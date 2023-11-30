4a5422bfde781d88b702d5bf828b41d36da15496
jsdriscoll8 <jsdriscoll8@gmail.com>
2023-11-30 15:51:39 -0500

    added Tim Thomas bio

--- timThomas.php
+++ timThomas.php
@@ -15,7 +15,16 @@
 <section id="bio">
     <h2> Biography </h2>
     <p>
+        Tim Thomas was born April 15, 1974 in Michigan. He played college hockey
+        for the University of Vermont for four years from 1993 to 1997; during this
+        time he was drafted by the Quebec Nordiques (now the Colorado Avalanche)
+        but did not accept the offer. During his college career, he went 81-43-15
+        with a 2.70 goals against average, and still holds the third spot for saves
+        in NCAA Division I history.<br>
 
+        Thomas began his starting role as goaltender for the Bruins in the 2006-07
+        NHL season. His best years came in 2008-09 and 2010-11, in which he posted
+        excellent .933 and .938 save percentages, respectively.
     </p>
 </section>
 

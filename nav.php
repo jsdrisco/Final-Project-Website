bef23292b5790057d3cc320517529876e8c59bd4
jsdriscoll8 <jsdriscoll8@gmail.com>
2023-11-30 16:25:10 -0500

    added nav.php, placed in top.php

---
+++ nav.php
@@ -0,0 +1,8 @@
+<nav>
+    <p>
+        <a href="bergeron.php"> Patrice Bergeron </a>
+        <a href="timThomas.php"> Tim Thomas </a>
+        <a href="main.php"> Home Page </a>
+        <a href="form.php"> Form </a>
+    </p>
+</nav>
--- top.php
+++ top.php
@@ -16,6 +16,7 @@
     </head>
     <body>
         <?php
+            include 'nav.php';
             include 'header.php';
         ?>
         <main>

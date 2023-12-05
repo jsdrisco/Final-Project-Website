a24f03a747f19114f72e70eef30aabc9fe551642
andrewsubie <asubach@uvm.edu>
2023-12-05 17:06:36 -0500

    stylin

--- nav.php
+++ nav.php
@@ -1,8 +1,32 @@
+
 <nav>
-    <p>
-        <a href="bergeron.php"> Patrice Bergeron </a>
-        <a href="timThomas.php"> Tim Thomas </a>
-        <a href="index.php"> Home Page </a>
-        <a href="form.php"> Form </a>
-    </p>
+        <a class="<?php
+        if ($pathParts['filename'] == "bergeron") {
+            print 'activePage';
+        }
+        ?>" href="bergeron.php"> Patrice Bergeron </a>
+
+        <a class="<?php
+        if ($pathParts['filename'] == "chara") {
+            print 'activePage';
+        }
+        ?>" href="chara.php"> Zdeno Chara </a>
+
+        <a class="<?php
+        if ($pathParts['filename'] == "timThomas") {
+            print 'activePage';
+        }
+        ?>" href="timThomas.php"> Tim Thomas </a>
+
+        <a class="<?php
+        if ($pathParts['filename'] == "index") {
+            print 'activePage';
+        }
+        ?>" href="index.php"> Home Page </a>
+
+        <a class="<?php
+        if ($pathParts['filename'] == "index") {
+            print 'activePage';
+        }
+        ?>" href="form.php"> Form </a>
 </nav>

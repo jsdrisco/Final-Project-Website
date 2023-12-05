7c4d95b0631e9cee8db8779551d427873757a71c
jsdriscoll8 <jsdriscoll8@gmail.com>
2023-12-05 18:16:52 -0500

    indentation

--- database-connect.php
+++ database-connect.php
@@ -1,10 +1,11 @@
 <!-- Connecting -->
 <?php
-$databaseName = 'JLACKEY_final';
-$dsn = 'mysql:host=webdb.uvm.edu;dbname='. $databaseName;
-$username = 'jlackey_writer';
-$password = ')/6R[P%vyn0R6q,zE0+f';
+    $databaseName = 'JLACKEY_final';
+    $dsn = 'mysql:host=webdb.uvm.edu;dbname='. $databaseName;
+    $username = 'jlackey_writer';
+    $password = ')/6R[P%vyn0R6q,zE0+f';
 
-$pdo = new PDO($dsn, $username, $password);
-if($pdo) print '<!-- Connection complete -->';
+    $pdo = new PDO($dsn, $username, $password);
+    if($pdo)
+        print '<!-- Connection complete -->';
 ?>
